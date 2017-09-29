dac14b05e5bf5aae70603d746da6c046b487841b
Ethan <etsmith@cpp.edu>
2017-09-29 00:18:56 -0700

    implemented multiple simutaneous clients

--- Exercises/Exercise 1/EchoServer.java
+++ Exercises/Exercise 1/EchoServer.java
@@ -9,23 +9,48 @@ public final class EchoServer {
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
+                try {
+                    Socket socket = serverSocket.accept();
                     String address = socket.getInetAddress().getHostAddress();
                     System.out.printf("Client connected: %s%n", address);
-                    
-                    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-                    BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                    String inputLine;
-                    while ((inputLine = br.readLine()) != null) {
-                        if (inputLine.equalsIgnoreCase("exit")) {
-                            System.out.printf("Client disconnected: %s%n", address);
-                            break;
-                        }
-                        pw.println("Server> " + inputLine);
-                    }
+                    EchoHelper eh = new EchoHelper(socket);
+                    eh.start();
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
             }
+        }
+    }
+    
+    static class EchoHelper extends Thread {
+        Socket client;
+        String address;
+        EchoHelper(Socket socket) {
+            this.client = socket;
+        }
+        
+        public void run() {
+            try {
+                PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String inputLine;
+                while ((inputLine = br.readLine()) != null) {
+                    if (inputLine.equalsIgnoreCase("exit")) {
+                        System.out.printf("Client disconnected: %s%n", address);
+                        break;
+                    }
+                    pw.println("Server> " + inputLine);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            
         }
     }
 }
